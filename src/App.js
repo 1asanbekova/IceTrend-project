00bba29d28dc052788a00a00a430af009f9e194b
roseaika05@gmail.com <roseaika05.gmail.com>
2022-11-29 11:48:04 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Navbar from "./Components/Navbar/Navbar";
+import UpNavbar from "./Components/Navbar/UpNavbar";
+
 function App() {
-  return <div className="App">lkn;knk</div>;
+  return (
+    <div className="App">
+      <UpNavbar />
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/UpNavbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpNavbar = () => {
+  return <div>Up Nabvar</div>;
+};
+
+export default UpNavbar;
